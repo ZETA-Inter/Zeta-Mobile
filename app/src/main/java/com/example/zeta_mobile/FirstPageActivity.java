8ad5e4833de751cd1ec0891e626de33bb2bc8120
LauraTavora <ltavora@jbssa.com>
2025-09-17 11:03:56 -0300

    build: initial part for login and registrer company

--- app/src/main/java/com/example/zeta_mobile/FirstPageActivity.java
+++ app/src/main/java/com/example/zeta_mobile/FirstPageActivity.java
@@ -1,24 +1,30 @@
 package com.example.zeta_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.zeta_mobile.R;
+import com.example.zeta_mobile.company.LoginCompanyActivity;
+import com.example.zeta_mobile.worker.LoginWorkerActivity;
+import com.google.android.material.button.MaterialButton;
 
 public class FirstPageActivity extends AppCompatActivity {
+
+    private MaterialButton btnFornecedor, btnProdutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_first_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnFornecedor = findViewById(R.id.btnFornecedor);
+        btnProdutor   = findViewById(R.id.btnProdutor);
+
+        btnFornecedor.setOnClickListener(v ->
+                startActivity(new Intent(this, LoginCompanyActivity.class)));
+
+        btnProdutor.setOnClickListener(v ->
+                startActivity(new Intent(this, LoginWorkerActivity.class)));
     }
 }
