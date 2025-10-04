a6b8d878ec3a099136ce8f82feb146d150bdf4d8
BiaClarindo <bianca.clarindo@germinare.org.br>
2025-10-04 11:17:37 -0300

    subindo o adapter no core

--- core/src/main/java/com/example/core/FirstPage.java
+++ core/src/main/java/com/example/core/FirstPage.java
@@ -1,5 +1,6 @@
 package com.example.core;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import com.example.core.TipoUsuario;
 
 import com.example.core.databinding.FragmentFirstPageBinding;
 
@@ -21,17 +23,22 @@ public class FirstPage extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentFirstPageBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
         // Navegação para o LoginFragment
-        binding.btnFornecedor.setOnClickListener(v ->
-                // Use o ID do fragmento de destino que foi definido no nav_core.xml
-                Navigation.findNavController(v).navigate(R.id.Login)
-        );
 
-        binding.btnProdutor.setOnClickListener(v ->
-                // Use o ID do fragmento de destino que você definiu no nav_core.xml
-                Navigation.findNavController(v).navigate(R.id.Login)
-        );
+
+        binding.btnFornecedor.setOnClickListener(v -> {
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("TIPO_USUARIO", TipoUsuario.COMPANY);
+            // Use o ID do fragmento de destino que foi definido no nav_core.xml
+            Navigation.findNavController(v).navigate(R.id.Login, bundle);
+        });
+
+        binding.btnProdutor.setOnClickListener(v -> {
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("TIPO_USUARIO", TipoUsuario.WORKER);
+            // Use o ID do fragmento de destino que você definiu no nav_core.xml
+            Navigation.findNavController(v).navigate(R.id.Login, bundle);
+        });
 
         return root;
     }
---
+++ core/src/main/java/com/example/core/TipoUsuario.java
@@ -0,0 +1,6 @@
+package com.example.core;
+
+public enum TipoUsuario {
+    WORKER,
+    COMPANY
+}
