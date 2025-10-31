b86e9a6a78ebd10e543129938bf0b95951696422
juliaporf <157625969+juliaporf@users.noreply.github.com>
2025-10-31 16:55:30 -0300

    arrumando o xml de tablet e celular de produtor, fornecedor e core

--- feature-produtor/src/main/java/com/example/feature_produtor/HomePageWorkerFragment.java
+++ feature-produtor/src/main/java/com/example/feature_produtor/HomePageWorkerFragment.java
@@ -296,7 +296,7 @@ public class HomePageWorkerFragment extends Fragment
 
         perfil.setOnClickListener(v -> nav.navigate(R.id.Profileworker));
         boxIa.setOnClickListener(v -> nav.navigate(R.id.ChatBotPageWorker));
-        iconConfig.setOnClickListener(v -> { });
+        iconConfig.setOnClickListener(v -> { nav.navigate(R.id.FlashCardStudy);});
         iconNotificacao.setOnClickListener(v -> nav.navigate(R.id.CardNotificacao));
 
 
