50a8620dd31ca62041123e996f47ebe9c87ca99b
juliaporf <157625969+juliaporf@users.noreply.github.com>
2025-11-02 18:59:08 -0300

    arrumando style

--- feature-produtor/src/main/java/com/example/feature_produtor/adapter/StepsLessonAdapter.java
+++ feature-produtor/src/main/java/com/example/feature_produtor/adapter/StepsLessonAdapter.java
@@ -11,13 +11,17 @@ import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.feature_produtor.R;
-import com.example.feature_produtor.model.mongo.Class; // IMPORTANTE: Importar o modelo Class
+import com.example.core.model.mongo.Class; // IMPORTANTE: Importar o modelo Class
 
 // Extende ListAdapter de Class
 public class StepsLessonAdapter extends ListAdapter<Class, StepsLessonAdapter.LessonStepViewHolder> {
 
     // A interface usa o modelo Class
     public interface OnStepClickListener {
+        void onProgressUpdated(int newPercentage);
+
+        void onError(String message);
+
         void onStepClick(Class item, Integer stepNumber);
     }
 
