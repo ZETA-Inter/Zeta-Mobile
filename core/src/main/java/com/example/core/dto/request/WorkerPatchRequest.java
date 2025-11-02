b74d0e485bce116357d5e324dbdcaed0f3fdb213
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-11-02 13:41:18 -0300

    fix: create the getter and constructor of WorkerPatchRequest

--- core/src/main/java/com/example/core/dto/request/WorkerPatchRequest.java
+++ core/src/main/java/com/example/core/dto/request/WorkerPatchRequest.java
@@ -15,5 +15,35 @@ public class WorkerPatchRequest {
     @SerializedName("company_id")
     private Integer companyId;
 
+    public WorkerPatchRequest() {}
+
+    public WorkerPatchRequest(String name, String email, String imageUrl, PlanInfoRequest planInfo, Integer companyId) {
+        this.name = name;
+        this.email = email;
+        this.imageUrl = imageUrl;
+        this.planInfo = planInfo;
+        this.companyId = companyId;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public PlanInfoRequest getPlanInfo() {
+        return planInfo;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public void setImageUrl(String url) { this.imageUrl = url; }
 }
