b309ed3377044dc77208096d24ceacf8e56828ca
LauraTavora <ltavora@jbssa.com>
2025-11-06 13:55:01 -0300

    fix: min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.zeta_mobile"
-        minSdk = 30
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
--- feature-fornecedor/build.gradle.kts
+++ feature-fornecedor/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        minSdk = 30
+        minSdk = 29
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
